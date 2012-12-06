673bb65c691b13b7b2b5ca7252fd9912637c9696
Thomas Pizon <thomas.pizon@etu.u-bordeaux1.fr>
2012-12-06 22:19:14 +0100

    Corrections

--- api/handler.py
+++ api/handler.py
@@ -3,18 +3,18 @@ import xml.etree.ElementTree as ElementTree
 
 from webapp2 import RequestHandler
 
-from models.node import Node
+from models.mapmarker import MapMarcker
 
-def get_nodes_by_zone(longitude, latitude, radius, limit=50):
+def get_map_markers_by_zone(longitude, latitude, radius, limit=50):
     query = Node.all()
     query.filter("longitude >", longitude - radius)
     query.filter("longitude <", longitude + radius)
     query.filter("latitude >", latitude - radius)
     query.filter("latitude <", latitude + radius)
     
-    for node in query.fetch(limit=limit):
-      if ((node.longitude - longitude) ** 2 + (node.latitude - latitude) ** 2 == radius ** 2):
-          yield node
+    for map_marker in query.fetch(limit=limit):
+      if ((map_marker.longitude - longitude) ** 2 + (map_marker.latitude - latitude) ** 2 == radius ** 2):
+          yield map_marker
 
 
 class ApiHandler(RequestHandler):
@@ -29,28 +29,26 @@ class ApiHandler(RequestHandler):
           
         limit = self.GET.get('limit', 50)
         
-        root_node = ElementTree.Element("root")
+        root = ElementTree.Element("root")
         
-        for node in get_nodes_by_zone(longitude, latitude, radius, limit):
-            root_node.append(node.toXML())
+        for map_marker in get_nodes_by_zone(longitude, latitude, radius, limit):
+            root_node.append(map_marker.to_element())
         
-        self.response.write(ElementTree.tostring(root_node, encoding="utf-8"))
+        self.response.write(ElementTree.tostring(root, encoding="utf-8"))
     
     def post(self):
         try:
-          node = Node(name=self.POST['name'],
-                      longitude=self.POST['longitude'],
-                      latitude=self.POST['latitude'])
+          map_marker = MapMarker(self.POST)
         except KeyError as e:
           self.response.status = 404
           return
         
-        node.put()
+        map_marker.put()
         
         self.response.status = 201
     
     def put(self):
-        node = Node.fromXML(self.body)
-        node.put()
+        map_marker = MapMarker.fromXML(self.body)
+        map_marker.put()
         
         self.response.status = 201
